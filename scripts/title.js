7e17acbd5a0908a44eb4024489de1b6012d7ecd7
Fair <82622753+SincerelyFair@users.noreply.github.com>
2022-12-15 01:07:39 +0100

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,41 +1,43 @@
-if (document['addEventListener']) {
-2
-    document['addEventListener']('DOMContentLoaded', function() {
-3
-        loaded()
-4
-    })
-5
-} else {
-6
-    if (document['attachEvent']) {
-7
-        document['attachEvent']('onreadystatechange', function() {
-8
-            loaded()
-9
-        })
-10
+var i=0;
+setInterval(function(){
+    var titles=[
+"sincerelyfair.cf",
+"sincerelyfair.c",
+"sincerelyfair.",
+"sincerelyfair",
+"sincerelyfai",
+"sincerelyfa",
+"sincerelyf",
+"sincerely",
+"sincerel",
+"sincere",
+"sincer",
+"since",
+"sinc",
+"sin",
+"si",
+"s",
+"si",
+"sin",
+"sin",
+"sinc",
+"since",
+"sincer",
+"sincere",
+"sincerel",
+"sincerely",
+"sincerelyf",
+"sincerelyfa",
+"sincerelyfai",
+"sincerelyfair",
+"sincerelyfair.",
+"sincerelyfair.c",
+"sincerelyfair.cf",
+]
+
+    if(i===titles.length) {
+        i=0;
     }
-11
-};
-12
-?
-13
-function loaded() {
-14
-    setInterval(loop, 350)
-15
-}
-16
-var x = 0;
-17
-var titleText = ['n', 'ni', 'nig', 'nigg', 'nigga', 'niggas', 'niggas.', 'niggas.l', 'niggas.lo', 'niggas.lol', 'niggas.lo', 'niggas.l', 'niggas.', 'niggas', 'nigga', 'nigg', 'nig', 'ni', 'n'];
-18
-?
-19
-function loop() {
-20
-    document['getElementsByTagName']('title')[0]['innerHTML'] = titleText[x++ % titleText['length']]
-21
-}
+    document.title = titles[i];
+    i++;
+}, 500);
